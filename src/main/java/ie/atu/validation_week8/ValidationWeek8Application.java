063d0b4c9b91232e6d9de8d690bccaa802f1140f
James a <g00379271@atu.ie>
2023-11-15 14:23:05 +0000

    Creation of project

---
+++ src/main/java/ie/atu/validation_week8/ValidationWeek8Application.java
@@ -0,0 +1,13 @@
+package ie.atu.validation_week8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationWeek8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationWeek8Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/validation_week8/ValidationWeek8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.validation_week8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationWeek8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
